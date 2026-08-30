ebdc70379b1e28ab07785c1934c90046a044e98c
deex2 <deex2@users.noreply.github.com>
2026-08-30 12:16:39 -0700

    chore: prepare v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "dev.herdroid"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
         testInstrumentationRunner = "dev.herdroid.core.testing.HerdroidTestRunner"
     }
 
